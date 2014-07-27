5aff52d01611ffc5b2ff708c29a223acd3787423
Ben Roberts <ben@sihnon.net>
2014-07-27 16:58:43 +0100

    Permit override of package name

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,13 +4,20 @@
 #
 # The package might not specify a dependency on Java, so you may need to
 # specify that yourself
-class jenkins::package {
+#
+# Parameters
+# [*package*]
+#   Specify the name of the jenkins package (defaults to 'jenkins').
+#   Useful for distributions that ship the package under a different name.
+class jenkins::package (
+    $package = 'jenkins',
+){
 
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  package { 'jenkins' :
+  package { $package:
     ensure => $::jenkins::version;
   }
 }
